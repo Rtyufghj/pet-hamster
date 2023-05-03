5b22d24116d16e16fc58f0aedb753967b0591f29
Rtyufghj <131329039+Rtyufghj@users.noreply.github.com>
2023-05-03 14:22:37 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    A.move(-1)
+    if (A.isTouching(B)) {
+        game.addScore(1)
+    }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(game.score())
+})
+input.onButtonPressed(Button.B, function () {
+    A.move(1)
+    if (A.isTouching(B)) {
+        game.addScore(1)
+    }
+})
+let B: game.LedSprite = null
+let A: game.LedSprite = null
+A = game.createSprite(2, 2)
+B = game.createSprite(4, 2)
+B.set(LedSpriteProperty.Brightness, 50)
+game.setScore(0)
+game.startCountdown(10000)
